9942c73fc4a900083d3ea7ae1ba1547089dbb597
Marcus Caetano <caetanomarcusf@gmail.com>
2023-12-21 15:56:42 -0300

    vamo que vamo

--- src/app/paginainicial/home/home.component.ts
+++ src/app/paginainicial/home/home.component.ts
@@ -1,7 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { DropdownModule } from 'primeng/dropdown'; 
+import { DropdownModule } from 'primeng/dropdown';
+
+interface IMenuItem {
+  name: string,
+  subItems: {
+    name: string;
+    path: string;
+  }[],
+  isOpen: boolean
+}
 
 @Component({
   selector: 'app-home',
@@ -12,22 +21,84 @@ import { DropdownModule } from 'primeng/dropdown';
 })
 export class HomeComponent {
 
+menuItems: IMenuItem[] = [
+  {
+    name: "All Sourses",
+    subItems: [
+      {
+        name: "Sofia",
+        path: ""
+      },
+      {
+        name: "Students",
+        path: "/alunos"
+      },
+    ],
+    isOpen: false
+  },
+  {
+    name: "Calendars",
+    subItems: [
 
+    ],
+    isOpen: false
+  },
+  {
+    name: "Registration System",
+    subItems: [
+      {
+        name: "Students",
+        path: "/alunos"
+      },
+      {
+        name: "Documents",
+        path: ""
+      },
+      {
+        name: "Fees",
+        path: ""
+      },
+
+    ],
+    isOpen: false
+  },
+  {
+    name: "Financial",
+    subItems: [
+
+    ],
+    isOpen: false
+  }
+]
 
 constructor(private router: Router){
 
 }
 
 
-visualizar(){
-  this.router.navigate(['/alunos'])
+visualizar(path: string){
+  this.router.navigate([path])
 }
 
 
 registrationSystem:boolean = false;
 
-clickRegistrationSystem(){
-  this.registrationSystem = true; 
+clickOpenMenu(name: string){
+  //pego o index do menu que tem esse nome
+  const index = this.menuItems.findIndex(element => element.name === name)
+  //mudo a variavel do menu que tem esse nome
+  this.menuItems[index].isOpen = !this.menuItems[index].isOpen
+
+  //se eu quiser fechar todas as outras, basta colocar a condicao de n ser a que abri e  setár elas pra false
+  this.menuItems.forEach(item => {
+    if(item.name !== name) {
+      item.isOpen = false;
+    }
+  })
+}
+
+ocultMenus(){
+  this.menuItems.forEach(item => item.isOpen = false)
 }
 
 
